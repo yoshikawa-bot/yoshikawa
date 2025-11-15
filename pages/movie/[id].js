b0a54efc8e5d22fc85f531a74a05b006aceaf437
Yoshikawa Project <ilogs.lya@gmail.com>
2025-11-15 15:21:00 -0300

    Update [id].js

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -296,19 +296,19 @@ const Header = () => (
 const BottomNav = ({ selectedPlayer, onPlayerChange, isFavorite, onToggleFavorite, onShowInfo }) => (
   <div className="bottom-nav-container streaming-mode">
     <div className="main-nav-bar">
+      {/* CORRIGIDO: Removida a tag <span>, o ícone é centralizado e a proporção é mantida pelo CSS */}
       <Link href="/" className="nav-item">
         <i className="fas fa-home"></i>
-        <span>Início</span>
       </Link>
       
+      {/* CORRIGIDO: Removida a tag <span>, o ícone é centralizado e a proporção é mantida pelo CSS */}
       <button className="nav-item" onClick={onShowInfo}>
         <i className="fas fa-info-circle"></i>
-        <span>Info</span>
       </button>
       
+      {/* CORRIGIDO: Removida a tag <span>, o ícone é centralizado e a proporção é mantida pelo CSS */}
       <button className={`nav-item ${isFavorite ? 'active' : ''}`} onClick={onToggleFavorite}>
         <i className={isFavorite ? 'fas fa-heart' : 'far fa-heart'}></i>
-        <span>Favorito</span>
       </button>
     </div>
     
